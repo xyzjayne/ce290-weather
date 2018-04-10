5b404533d07b7214e771c3a523b7ab77e108be18
xyzjayne <orzmath@gmail.com>
2018-04-09 19:21:48 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import webapp2
-#import networkx as nx
-#G = nx.fast_gnp_random_graph(100,10)
-#n = G.number_of_nodes()
+
 class MainPage(webapp2.RequestHandler):
     def get(self):
         self.response.headers['Content-Type'] = 'text/plain'
