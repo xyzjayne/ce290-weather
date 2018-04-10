8f7ff3ca6688437e4e2cb53bac03b355f1372aa5
xyzjayne <orzmath@gmail.com>
2018-04-09 19:45:04 -0700

    Update appengine_config.py

--- appengine_config.py
+++ appengine_config.py
@@ -2,5 +2,6 @@
 from google.appengine.ext import vendor
 import os
 # Add any libraries install in the "lib" folder.
-vendor.add(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'lib'))
-#vendor.add('/home/orzmath/src/ce290-weather/lib')
+#vendor.add(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'lib'))
+vendor.add(os.path.join("~", "src/ce290-weather",'lib'))
+ 
