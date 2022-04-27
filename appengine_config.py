72c676e0c20b26ef78aac8b2c7f46181ddfe41fd
removedporn <86824510+removedporn@users.noreply.github.com>
2022-04-28 06:59:35 +0800

    Delete appengine_config.py

--- appengine_config.py
+++
@@ -1,7 +0,0 @@
-# appengine_config.py
-from google.appengine.ext import vendor
-import os
-# Add any libraries install in the "lib" folder.
-#vendor.add(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'lib'))
-#vendor.add(os.path.join("/home/orzmath/", "src/ce290-weather",'lib'))
- 
